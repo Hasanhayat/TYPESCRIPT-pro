f56449e727d8d9e073e829a7e90ffc9f1eb9e7c7
hasanhayat <aylskhsmvz.v@gmail.com>
2025-04-14 22:08:46 +0500

    ts5

--- script.js
+++ script.js
@@ -1,3 +1,26 @@
 var a = "Hello, world!";
 var b = "Hello, world!";
 var c = "Hello, world!";
+// ts function to check if two strings are equal
+function areStringsEqual(str1, str2) {
+    return str1 === str2;
+}
+// ts object
+var person = {
+    name: "John",
+    age: 30
+};
+// ts class
+var PersonClass = /** @class */ (function () {
+    function PersonClass(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+    // ts method to get person info
+    PersonClass.prototype.getInfo = function () {
+        return "".concat(this.name, " is ").concat(this.age, " years old.");
+    };
+    return PersonClass;
+}());
+var personClass = new PersonClass("John", 30);
+console.log(personClass.getInfo());
--- script.ts
+++ script.ts
@@ -3,3 +3,42 @@ let b:string = "Hello, world!";
 let c:string = "Hello, world!";
 
 
+// ts function to check if two strings are equal
+function areStringsEqual(str1: string, str2: string): boolean {
+    return str1 === str2;
+}
+
+// ts interface
+interface Person {
+    name: string;
+    age: number;
+}       
+
+
+
+// ts object
+let person: Person = {
+    name: "John",
+    age: 30
+}
+
+
+// ts class
+class PersonClass {
+    name: string;
+    age: number;
+
+    constructor(name: string, age: number) {
+        this.name = name;
+        this.age = age;
+    }
+
+    // ts method to get person info
+    getInfo(): string {
+        return `${this.name} is ${this.age} years old.`;
+    }
+}
+let personClass = new PersonClass("John", 30);
+console.log(personClass.getInfo());
+
+
